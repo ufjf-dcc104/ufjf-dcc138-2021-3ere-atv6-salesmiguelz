246f32c455b67d16859ade4ab7eb6e7d50426533
Miguel Sales <miguelsales02@gmail.com>
2022-01-11 16:58:10 -0300

    Adiciona passo a Cena e Sprites

--- js/Cena.js
+++ js/Cena.js
@@ -17,4 +17,10 @@ export default class Cena{
     adicionar(sprite){
         this.sprites.push(sprite);
     }
+
+    passo(dt){
+        for (const sprite of this.sprites) {
+            sprite.passo(dt);
+        }
+    }
 }
--- js/Sprite.js
+++ js/Sprite.js
@@ -1,14 +1,23 @@
 export default class Sprite{
     //Responsavel por modelar algo que se move na tela.
-    constructor({x=100, y=100, w=20, h=20, color="white"}={}){
+    constructor({x=100, y=100, vx=0, vy=0, w=20, h=20, color="white"}={}){
         this.x = x;
         this.y = y;
+
+        this.vx = vx;
+        this.vy = vy;
+
         this.w = w;
         this.h = h;
+
         this.color = color;
     }
     desenhar(ctx){
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y,this.w, this.h);
+    }
+    passo(dt){
+        this.x = this.x + this.vx * dt;
+        this.y = this.y + this.vy * dt;
     }
 }
