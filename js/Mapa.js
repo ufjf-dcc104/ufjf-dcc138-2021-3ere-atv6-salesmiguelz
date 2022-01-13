d02fbf406b0a133874254b307a99660085d96bbe
Miguel Sales <miguelsales02@gmail.com>
2022-01-13 11:48:56 -0300

    Adiciona mais um exemplo de tipo de tile

--- js/Mapa.js
+++ js/Mapa.js
@@ -27,6 +27,14 @@ export default class Mapa{
                         ctx.strokeStyle = "black";
                         ctx.strokeRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
                         break;
+                    case 2:
+                        ctx.fillStyle = "orange";
+                        ctx.fillRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
+
+                        ctx.lineWidth = 1;
+                        ctx.strokeStyle = "red";
+                        ctx.strokeRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
+                        break;
                     default:
                         ctx.fillStyle = "black";
                         ctx.fillRect(c * this.SIZE, l * this.SIZE, this.SIZE, this.SIZE);
